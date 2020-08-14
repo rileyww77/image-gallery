ddca92ab60be43fd7da0c549d4d1c438e6a19942
riley_ww77 <riley_ww77@yahoo.com>
2020-08-14 16:40:26 -0500

    added get statement

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    myPictures: []
+  }
+
+  getGallery = () => {
+
+    axios.get('/gallery')
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          myPictures: response.data
+        })
+      }).catch((error) => {
+        console.log(error)
+      })
+
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
       </div>
     );
   }
