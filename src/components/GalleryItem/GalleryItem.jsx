3d17be5a6d61cd9418f4785abd3a1ebeaed2c998
riley_ww77 <riley_ww77@yahoo.com>
2020-08-15 12:02:54 -0500

    got toggling function working

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 class GalleryItem extends Component {
 
-    
+    state = {
+        imageOn: true,
+    }    
+
+    toggleDesription = () => {
+        this.setState({
+            imageOn: !this.state.imageOn,
+           
+        })
+        
+    }
 
     render() {
+        
+
+        console.log(this.props.picture.description)
         return (
             <>
-                <p>{this.props.picture.path}</p>
+            {this.state.imageOn ? <img onClick={this.toggleDesription} src={this.props.picture.path}/> : <p onClick={this.toggleDesription}> {this.props.picture.description} </p>}
                 <button onClick={(event) => this.props.addLike(this.props.picture.id)}>like!</button>
                 {this.props.picture.likes}
                 
